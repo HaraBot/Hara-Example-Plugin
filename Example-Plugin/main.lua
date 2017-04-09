2209b719bd7a36ce646349ccb09aad3d4aa17426
James Upjohn <jspartan250@gmail.com>
2017-04-09 19:51:51 +1200

    Updated mappings

--- Example-Plugin/main.lua
+++ Example-Plugin/main.lua
@@ -1,17 +1,50 @@
 -- This plugin is for example purpose only, but you may modify the code to suit yourself.
+--[[
 
-bot.enablePlugin(function()
-    bot.log("info", bot:getDescription():getName() .. " has been enabled!")
+	API Mapping:
+
+	bot.prefix 
+	bot.getClient()
+	bot.requestBuffer(function())
+	plugin.getDescription().*
+	plugin.registerCommand(name, usage, function(message, args))
+	plugin.pluginEnable(function())
+	plugin.pluginDisable(function())
+	plugin.registerEvent(eventName, function(event))
+	emoji.getAlias(unicodeEmoji)
+	emoji.getEmoji(alias)
+	logger.info(message)
+	logger.warn(message)
+	logger.error(message)
+	logger.debug(message)
+
+]]--
+
+plugin.pluginEnable(function()
+	logger.info(plugin.getDescription():getName() .. " has been enabled!")
 end)
 
-bot.disablePlugin(function()
-    bot.log("info", bot:getDescription():getName() .. " has been disabled! See you later.")
+plguin.pluginDisable(function()
+	logger.info(plugin.getDescription():getName() .. " has been disabled! See you later.")
 end)
 
 bot.registerCommand("hello", "hello [name]", function(m, args)
-    if args[1] ~= nil then
-        m.channel:sendMessage("Hello there <@" .. args[1] .. ">")
-    else
-        m.channel:sendMessage("Hello <@" .. m.author.name .. ">")
-    end
+	if args[1] ~= nil then
+		bot.requestBuffer(function()
+			-- If the client is being rate limited the message will be sent after the bucket clears.
+			m:getChannel():sendMessage("Hello there " .. args[1])
+		end)
+	else
+		bot.requestBuffer(function()
+			m:getChannel():sendMessage("Hello " .. m:getMessage():getAuthor():getID())
+		end)
+	end
+end)
+
+bot.registerEvent(events.ReactionAddEvent, function(event)
+	if emoji.getString(event:getReaction():toString()) == ":+1:" and event:getMessage():getAuthor():getID() == bot.getClient():getOurUser():getID() then
+		bot.requestBuffer(function()
+			event:getMessage():getChannel():sendMessage("I'm pleased you liked it " .. event:getUser():mention())
+		end)
+	end
 end)
